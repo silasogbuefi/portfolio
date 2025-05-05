0d26816e1672046d3f6d985fd8447c5771aa466b
segun adebayo <eniolaadebayo24@gmail.com>
2025-05-05 21:53:10 +0100

    initial commit for dev portfolio

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,32 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
 
+  darkMode: 'selector',
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+   
+};
 export default config;
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+
+//   theme: {
+//     extend: {
+//       colors: {
+//         lightHover: '#fcf4ff',
+//         darkHover: '#2a004a',
+//         darkTheme: '#11001F'
+//       },
+//       fontFamily: {
+//         Outfit: ["Outfit", "sans-serif"],
+//         Ovo: ["Ovo", "serif"]
+//       }
+//     }
+//   },
+
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
